94e61f04a4f7a7dcca228f0448ffd12efd67e210
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-11-03 00:09:18 +0600

    first commit

---
+++ src/Routess/Routess.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+import Checkout from "../Pages/Checkout/Checkout";
+import Bookings from "../Pages/Bookings/Bookings";
+import PrivateRoute from "../Pages/PrivateROute/PrivateRoute";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/signin',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
+            {
+                path: '/checkout/:id',
+                element: <Checkout></Checkout>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: '/bookings',
+                element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
+            }
+        ]
+    },
+]);
+
+
+export default router;
